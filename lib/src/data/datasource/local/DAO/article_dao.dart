53d20933c909e196c60a3bab24d05b74071063c9
Enes Akbal <enesakbal00@gmail.com>
2023-07-20 16:00:34 +0300

    Update article_dao.dart

--- lib/src/data/datasource/local/DAO/article_dao.dart
+++ lib/src/data/datasource/local/DAO/article_dao.dart
@@ -3,10 +3,8 @@ import 'package:floor/floor.dart';
 import '../../../../core/constants/db_constants.dart';
 import '../../../../domain/entities/_lite/lite_article_entity.dart';
 
-/// This class is a data access object (DAO) that allows us to query the database for playlist tracks
 @dao
 abstract class ArticleDao {
-  /// A query that returns a list of LocalPlaylistTrack objects.
   @Query('SELECT * from ${DBConstants.tableName}')
   Future<List<LiteArticleEntity>?> getBookmarkedArticles();
 
